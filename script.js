39a761f14def9426cb90d5746881c98b52daeb0f
rsudak <rsudack@yandex.com>
2024-07-10 20:30:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Функция для загрузки экранов
+    function loadScreen(screen) {
+        fetch(screen)
+            .then(response => response.text())
+            .then(html => {
+                document.getElementById('content').innerHTML = html;
+                // Добавьте вызовы скриптов, необходимых для каждого экрана, если они есть
+            })
+            .catch(error => {
+                console.error('Ошибка загрузки экрана:', error);
+            })
+    }
+
+    // Загрузка начального экрана (например, логин)
+    loadScreen('login/login.html')
+
+    // Пример для смены экрана
+    // Вы можете использовать события, такие как нажатие кнопок, для вызова этой функции
+    document.getElementById('someButton').addEventListener('click', function() {
+        loadScreen('calculate/calculate.html')
+    })
+})
+
